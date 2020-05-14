3223848575f4efeeeb766c76a9202efcdbbc7f46
barteksmolec12 <barteksmolec@o2.pl>
2020-05-14 11:20:08 +0200

    add all model classes

--- TrackerLibrary/TrackerLibrary/TournamentModel.cs
+++ TrackerLibrary/TrackerLibrary/TournamentModel.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
-	class TournamentModel
+	public class TournamentModel
 	{
 		public string TournamentName { get; set; }
 		public decimal EntryFee { get; set; }
-	    public List<TeamModel> EnteredTeams { get; set; }
-		public List<PrizeModel> Prizes { get; set; }
-		public List<List<MatchupModel>> Rounds { get; set; }
+		public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+		public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+		public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
 
-	}
+	}	
 
 
 }
