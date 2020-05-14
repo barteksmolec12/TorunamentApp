e713c340e6603cac2a0b824b1d998e0cc028ed0e
barteksmolec12 <barteksmolec@o2.pl>
2020-05-14 10:45:53 +0200

    update tournamentModel

--- TrackerLibrary/TrackerLibrary/TournamentModel.cs
+++ TrackerLibrary/TrackerLibrary/TournamentModel.cs
@@ -8,5 +8,13 @@ namespace TrackerLibrary
 {
 	class TournamentModel
 	{
+		public string TournamentName { get; set; }
+		public decimal EntryFee { get; set; }
+	    public List<TeamModel> EnteredTeams { get; set; }
+		public List<PrizeModel> Prizes { get; set; }
+		public List<List<MatchupModel>> Rounds { get; set; }
+
 	}
+
+
 }
