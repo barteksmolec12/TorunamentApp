5e174b2044ed53d8863557a1b717edda274d6f81
barteksmolec12 <barteksmolec@o2.pl>
2020-05-16 08:52:37 +0200

    clean files

--- TrackerLibrary/TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
+using TrackerLibrary.Models;
 
 namespace TrackerLibrary
-{
-	public class TextConnection : IDataConnection
+{ 
+	public class TextConnector : IDataConnection
 	{
 		//TODO - Wire up the CreatePrize for text files.
 		public PrizeModel CreatePrize(PrizeModel model)
--- TrackerLibrary/TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/TrackerLibrary/GlobalConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
@@ -22,7 +23,7 @@ namespace TrackerLibrary
 			{
 				//do something
 				//TODO - Create the text file connection
-				TextConnection text = new TextConnection();
+				TextConnector text = new TextConnector();
 				Connections.Add(text);
 			}
 				
